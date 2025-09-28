b41c9c3342b28d3a1ad51ba516f4fd58aaaf6255
Antoni Duda <antoni.duda@programmer.net>
2025-09-28 14:08:03 +0200

    feat(cmd/utd): Allowing port selection

--- cmd/utd/utd.go
+++ cmd/utd/utd.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/avalanche-pwn/utd/pkg/srv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: utd PORT")
+		return
+	}
+	port := os.Args[1]
+
 	var client srv.ClientSrv
-	client.Initialize("127.0.0.1:44453")
+	err := client.Initialize(fmt.Sprintf("127.0.0.1:%s", port))
+	if err != nil {
+		fmt.Println("Couldn't connect")
+		return
+	}
+	defer client.Close()
+
 	client.InitializeDAP()
 	client.Test()
-	defer client.Close()
 }
